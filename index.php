e35aeb9c3615294c3d8ebedc48a8e41d34a9e267
Marie Gandrille <mgandrille@yahoo.fr>
2020-05-18 11:58:01 +0200

    :sparkle: new config (add & require)

--- index.php
+++ index.php
@@ -1,3 +1,24 @@
 <?php
 
-require __DIR__ . 'config/config.php';
+require __DIR__ . '/config/config.php';
+
+$simba = new Animal;
+$simba->setId(1);
+$simba->setSpecies('Lion');
+$simba->setCountry('Afrique');
+
+var_dump($simba);
+
+$zooDeLaPlaine = new Zoo;
+$zooDeLaPlaine->setId(1);
+$zooDeLaPlaine->setName('Zoo de La Plaine');
+$zooDeLaPlaine->setCity('La Plaine');
+
+var_dump($zooDeLaPlaine);
+
+$firstAnimal = new AnimalZoo;
+$firstAnimal->setId(1);
+$firstAnimal->setAnimalId(1);
+$firstAnimal->setZooId(1);
+
+var_dump($firstAnimal);
