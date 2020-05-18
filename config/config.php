3bcb0e7f30975f680325aa9a8249146d5aa1f24b
Marie Gandrille <mgandrille@yahoo.fr>
2020-05-18 12:13:09 +0200

    :sparkle: Import auto des classes dans src

--- config/config.php
+++ config/config.php
@@ -1,5 +1,10 @@
 <?php
 
-require __DIR__ . '/../src/models/Animal.php';
-require __DIR__ . '/../src/models/Zoo.php';
-require __DIR__ . '/../src/models/AnimalZoo.php';
+// require __DIR__ . '/../src/models/Animal.php';
+// require __DIR__ . '/../src/models/Zoo.php';
+// require __DIR__ . '/../src/models/AnimalZoo.php';
+
+spl_autoload_register(function ($class) {
+    include __DIR__ . '/../src/models/' . $class . '.php';
+    include __DIR__ . '/../src/controllers/' . $class . '.php';
+});
