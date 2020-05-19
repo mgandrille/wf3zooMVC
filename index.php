b17f9c7519372e2c4dfab0e11005510759437c0f
Marie Gandrille <mgandrille@yahoo.fr>
2020-05-19 11:11:40 +0200

    Création de routes via router

--- index.php
+++ index.php
@@ -1,28 +1,52 @@
 <?php
 
+use Bramus\Router\Router;
+
 require __DIR__ . '/vendor/autoload.php';
 require __DIR__ . '/config/config.php';
 
+$router = new Router;
 
+$router->get('/about', function() {
+    echo "bienvenue sur la page About!";
+});
 
-$simba = new Animal;
-$simba->setId(1);
-$simba->setSpecies('Lion');
-$simba->setCountry('Afrique');
+$router->get('/contact', function () {
+    echo "Page contactez-nous.";
+});
 
-var_dump($simba);
+$router->get('/conditions', function() {
+    echo 'Voici les conditions d\'utilisation.';
+});
 
-$zooDeLaPlaine = new Zoo;
-$zooDeLaPlaine->setId(1);
-$zooDeLaPlaine->setName('Zoo de La Plaine');
-$zooDeLaPlaine->setCity('La Plaine');
+$router->get('/articles/d{id}', function($id) {
+    echo 'Voici l\'article numéro ' . $id;
+});
 
-var_dump($zooDeLaPlaine);
+$router->get('/product/([a-z0-9_-]+)', function($product) {
+    echo 'Voici le produit demandé : ' . htmlentities($product) ;
+});
 
-$firstAnimal = new AnimalZoo;
-$firstAnimal->setId(1);
-$firstAnimal->setAnimalId(1);
-$firstAnimal->setZooId(1);
+$router->run();
 
-var_dump($firstAnimal);
+// $simba = new Animal;
+// $simba->setId(1);
+// $simba->setSpecies('Lion');
+// $simba->setCountry('Afrique');
+
+// var_dump($simba);
+
+// $zooDeLaPlaine = new Zoo;
+// $zooDeLaPlaine->setId(1);
+// $zooDeLaPlaine->setName('Zoo de La Plaine');
+// $zooDeLaPlaine->setCity('La Plaine');
+
+// var_dump($zooDeLaPlaine);
+
+// $firstAnimal = new AnimalZoo;
+// $firstAnimal->setId(1);
+// $firstAnimal->setAnimalId(1);
+// $firstAnimal->setZooId(1);
+
+// var_dump($firstAnimal);
 
